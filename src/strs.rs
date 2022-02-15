68cba36d086cb331f09196322919fe827a0da6ca
chernyshev-alexander <achernyshev@griddynamics.com>
2022-02-15 23:17:20 +0100

    length_of_longest_substring_opt added 2

--- src/strs.rs
+++ src/strs.rs
@@ -43,25 +43,20 @@ impl Solution {
 
   // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-  pub fn length_of_longest_substring_bf(s: String) -> i32 {
+  pub fn length_of_longest_substring_opt(s: String) -> i32 {
     if s.len() <= 1 {
       return s.len() as i32;
     };
-    let mut result = 0;
-    let b = s.as_bytes();
-    for i in 0..b.len() {
-      let (mut partial_len, mut seen_chars) = (0, [0; 255]);
 
-      for j in i..b.len() {
-        let c = b[j] as usize;
-        if seen_chars[c] == 0 {
-          seen_chars[c] = c;
-          partial_len += 1;
-          result = std::cmp::max(result, partial_len);
-        } else {
-          break;
-        }
+    let (mut result, mut l, b, mut seen) = (0, 0, s.as_bytes(), [u16::MAX; 255]);
+    for r in 0..b.len() {
+      let c = b[r];
+      let seen_idx = seen[c as usize];
+      if seen_idx >= l && seen_idx != u16::MAX {
+        l = seen_idx + 1;
       }
+      seen[c as usize] = r as u16;
+      result = std::cmp::max(result, (r as i32 - l as i32) +1);
     }
     result
   }
@@ -72,20 +67,55 @@ mod test {
   use crate::strs::Solution;
 
   #[test]
-  fn length_of_longest_substring_case1() {
-    let result = Solution::length_of_longest_substring_bf("abcdabcdef".to_string());
+  fn length_of_longest_substring_case7() {
+    let result = Solution::length_of_longest_substring_opt("dvdf".to_string());
+    assert_eq!(result, 3);
+  }
+
+  #[test]
+  fn length_of_longest_substring_case6() {
+    let result = Solution::length_of_longest_substring_opt("abba".to_string());
     assert_eq!(result, 2);
   }
 
   #[test]
-  fn length_of_longest_substring_space() {
-    let result = Solution::length_of_longest_substring_bf(" ".to_string());
+  fn length_of_longest_substring_case5() {
+    let result = Solution::length_of_longest_substring_opt("bbb".to_string());
+    assert_eq!(result, 1);
+  }
+  #[test]
+  fn length_of_longest_substring_case4() {
+    let result = Solution::length_of_longest_substring_opt("123".to_string());
+    assert_eq!(result, 3);
+  }
+
+  #[test]
+  fn length_of_longest_substring_case4_1() {
+    let result = Solution::length_of_longest_substring_opt("au".to_string());
+    assert_eq!(result, 2);
+  }
+
+  #[test]
+  fn length_of_longest_substring_case3() {
+    let result = Solution::length_of_longest_substring_opt("abcabcbb".to_string());
+    assert_eq!(result, 3);
+  }
+
+  #[test]
+  fn length_of_longest_substring_case2() {
+    let result = Solution::length_of_longest_substring_opt("abcdabcdef".to_string());
+    assert_eq!(result, 6);
+  }
+
+  #[test]
+  fn length_of_longest_substring_case1() {
+    let result = Solution::length_of_longest_substring_opt(" ".to_string());
     assert_eq!(result, 1);
   }
 
   #[test]
-  fn length_of_longest_substring() {
-    let result = Solution::length_of_longest_substring_bf("pwwkew".to_string());
+  fn length_of_longest_substring_case0() {
+    let result = Solution::length_of_longest_substring_opt("pwwkew".to_string());
     assert_eq!(result, 3);
   }
 
