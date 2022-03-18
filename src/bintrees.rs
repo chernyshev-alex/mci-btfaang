1943ad9598b503feaae89d9eaa22d5641a8a9053
chernyshev-alexander <achernyshev@griddynamics.com>
2022-03-18 14:34:07 +0100

    right_side_view task

--- src/bintrees.rs
+++ src/bintrees.rs
@@ -1,4 +1,4 @@
-use std::cell::{RefCell};
+use std::cell::RefCell;
 use std::collections::VecDeque;
 use std::rc::Rc;
 
@@ -22,10 +22,11 @@ impl TreeNode {
 
 fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     fn max_depth_int(node: Option<&Rc<RefCell<TreeNode>>>, depth: i32) -> i32 {
-        if let Some(cell) = node { 
+        if let Some(cell) = node {
             std::cmp::max(
-            max_depth_int(cell.as_ref().borrow().left.as_ref(), 1 + depth), 
-            max_depth_int(cell.as_ref().borrow().right.as_ref(), 1+ depth))
+                max_depth_int(cell.as_ref().borrow().left.as_ref(), 1 + depth),
+                max_depth_int(cell.as_ref().borrow().right.as_ref(), 1 + depth),
+            )
         } else {
             depth
         }
@@ -34,16 +35,15 @@ fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
 }
 
 fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
-
     let mut res: Vec<Vec<i32>> = vec![];
     let mut q = &mut VecDeque::<Rc<RefCell<TreeNode>>>::new();
 
     if let Some(n) = root {
         q.push_front(n);
     }
-    
+
     while q.len() > 0 {
-        let mut level : Vec<i32>  = vec![];
+        let mut level: Vec<i32> = vec![];
         let mut next_level: Vec<Rc<RefCell<TreeNode>>> = vec![];
         while let Some(n) = q.pop_front() {
             let x = n.as_ref().borrow();
@@ -59,7 +59,35 @@ fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
         }
         res.push(level);
         next_level.iter().for_each(|e| q.push_back(e.clone()));
-    }       
+    }
+    res
+}
+
+pub fn right_side_view_bfs(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    let mut res: Vec<i32> = vec![];
+    let mut q = &mut VecDeque::<Rc<RefCell<TreeNode>>>::new();
+    if let Some(n) = root {
+        q.push_front(n);
+    }
+    let mut val = 0;
+    while q.len() > 0 {
+        let (mut cnt, qlen) = (0, q.len());
+        while cnt < qlen {
+            if let Some(n) = q.pop_front() {
+                let x = n.as_ref().borrow();
+                val = x.val;
+                let (l, r) = (x.left.clone(), x.right.clone());
+                if l.is_some() {
+                    q.push_back(l.unwrap());
+                }
+                if r.is_some() {
+                    q.push_back(r.unwrap());
+                }
+            }
+            cnt += 1;
+        }
+        res.push(val);
+    }
     res
 }
 
@@ -70,13 +98,26 @@ mod test {
     // https://leetcode.com/problems/maximum-depth-of-binary-tree/
     #[test]
     fn max_depth_test() {
-        let tree = Some(Rc::new(RefCell::new(TreeNode { val : 3,
-            left: Some(Rc::new(RefCell::new(TreeNode { val: 9, left: None, right: None}))),
-            right: Some(Rc::new(RefCell::new(
-                TreeNode { val: 20, 
-                    left: Some(Rc::new(RefCell::new(TreeNode { val : 15, left: None, right: None}))),  
-                    right: Some(Rc::new(RefCell::new(TreeNode { val : 20, left: None, right: None}))),
+        let tree = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 9,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 20,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: None,
                 }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 20,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
         })));
         assert_eq!(3, max_depth(tree));
     }
@@ -84,14 +125,57 @@ mod test {
     // https://leetcode.com/problems/binary-tree-level-order-traversal/
     #[test]
     fn level_order_test() {
-        let tree = Some(Rc::new(RefCell::new(TreeNode { val : 3,
-            left: Some(Rc::new(RefCell::new(TreeNode { val: 9, left: None, right: None}))),
-            right: Some(Rc::new(RefCell::new(
-                TreeNode { val: 20, 
-                    left: Some(Rc::new(RefCell::new(TreeNode { val : 15, left: None, right: None}))),  
-                    right: Some(Rc::new(RefCell::new(TreeNode { val : 7, left: None, right: None}))),
+        let tree = Some(Rc::new(RefCell::new(TreeNode {
+            val: 3,
+            left: Some(Rc::new(RefCell::new(TreeNode {
+                val: 9,
+                left: None,
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {
+                val: 20,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 15,
+                    left: None,
+                    right: None,
                 }))),
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 7,
+                    left: None,
+                    right: None,
+                }))),
+            }))),
         })));
-        assert_eq!(vec![vec![3],vec![9,20],vec![15,7]], level_order(tree));
+        assert_eq!(vec![vec![3], vec![9, 20], vec![15, 7]], level_order(tree));
+    }
+
+    // https://leetcode.com/problems/binary-tree-right-side-view/
+    #[test]
+    fn right_side_view_test() {
+        let tree = None;
+        assert_eq!(vec![] as Vec<i32>, right_side_view_bfs(tree));
+
+        let tree = Some(Rc::new(RefCell::new(TreeNode {
+            val: 1,
+            left: Some(Rc::new(RefCell::new(TreeNode {val: 2,
+                left: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 5, left: None, right: None,
+                }))),
+                right: None,
+            }))),
+            right: Some(Rc::new(RefCell::new(TreeNode {val: 3,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode {
+                    val: 4, left: None, right: None,
+                }))),
+            }))),
+        })));
+        assert_eq!(vec![1,3,4], right_side_view_bfs(tree));
+
+        let tree = Some(Rc::new(RefCell::new(TreeNode { 
+            val: 1, left: None,
+            right: Some(Rc::new(RefCell::new(TreeNode { val: 3, left: None, right: None })))
+            })));
+        assert_eq!(vec![1,3], right_side_view_bfs(tree));
     }
 }
